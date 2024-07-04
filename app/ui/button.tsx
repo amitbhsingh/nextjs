78de3427fc83d8086c0bcb739cd6f597ad95c285
amitbhsingh <amit.bh.singh@gmail.com>
2024-07-03 22:30:23 -0400

    chapter 12: mutating data

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,4 +1,6 @@
 import clsx from 'clsx';
+import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -6,14 +8,14 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function Button({ children, className, ...rest }: ButtonProps) {
   return (
-    <button
-      {...rest}
+    <Link
+    href={`/dashboard/invoices/${id}/edit`}
       className={clsx(
         'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
         className,
       )}
     >
       {children}
-    </button>
+    </Link >
   );
 }
